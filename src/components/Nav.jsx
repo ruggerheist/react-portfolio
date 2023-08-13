fde77fb46dd97dc91f66ca57180f24894f13aae0
Rick Heist <rick.heist@gmail.com>
2023-08-13 16:09:36 -0400

    continued styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,14 @@
+// import styles from './css/nav.module.css';
+import React from 'react';
+
 export default function Nav({setpage}) {
     return (
         <nav>
-            <ul>
-                <li onClick={() => setpage('About')}> About ME </li>
-                <li onClick={() => setpage('Portfolio')}> Portfolio </li>
-                <li onClick={() => setpage('Contact')}> Contact </li>
-                <li onClick={() => setpage('Resume')}> Resume </li>
+            <ul class="nav nav-justified">
+                <li class="nav-link" onClick={() => setpage('About')}> About ME </li>
+                <li class="nav-link" onClick={() => setpage('Portfolio')}> Portfolio </li>
+                <li class="nav-link" onClick={() => setpage('Contact')}> Contact </li>
+                <li class="nav-link" onClick={() => setpage('Resume')}> Resume </li>
             </ul>
         </nav>        
     )
